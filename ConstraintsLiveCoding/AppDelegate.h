8e6209086140c4dc34b3dd981f6ecc3489615cd6
mscline <mychiloscline@gmail.com>
2018-07-02 21:45:14 -0700

    Initial Commit

---
+++ ConstraintsLiveCoding/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ConstraintsLiveCoding
+//
+//  Created by xcode on 7/2/18.
+//  Copyright © 2018 MSCline. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ConstraintsLiveCoding/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ConstraintsLiveCoding
+//
+//  Created by xcode on 7/2/18.
+//  Copyright © 2018 MSCline. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
