42e3d593af5ddf3b3dd01f8e241f138304b0a840
mscline <mychiloscline@gmail.com>
2018-07-05 12:47:18 -0700

    Updated tableView cell.

--- ConstraintsLiveCoding/ViewControllerWithTable.m
+++ ConstraintsLiveCoding/ViewControllerWithTable.m
@@ -31,13 +31,17 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
 
     TableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:@"abc" forIndexPath:indexPath];
+    cell.labelA.text = @"Josh Smith";
+    cell.labelB.text = @"The Hammer";
+    cell.labelC.text = @"June, 2007";
     cell.label.text = self.sampleData[indexPath.row];
     return cell;
 }
 
 -(void)createSampleData {
     self.sampleData = @[@"Some text.",
-                        @"Lots of text that wraps and wraps and wraps a", @"There’s nothing worse than being stuck by yourself on a big holiday while everyone else is throwing parties. It can even happen to famous basketball players, like Jazz star Donovan Mitchell. When Mitchell was left without Fourth of July plans, he put out a call on Twitter.",
+                        @"Lots of text that wraps and wraps and wraps a",
+                        @"There’s nothing worse than being stuck by yourself on a big holiday while everyone else is throwing parties. It can even happen to famous basketball players, like Jazz star Donovan Mitchell. When Mitchell was left without Fourth of July plans, he put out a call on Twitter.",
                         @"Some text",
                         @"More text."];
 }
